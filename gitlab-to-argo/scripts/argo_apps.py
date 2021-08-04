33397aa5865815c75582ccf2d0ea4acaf0b4e49a
Matthew Duncan <maduncan@redhat.com>
2021-08-04 12:04:53 +0000

    Fix indent

--- gitlab-to-argo/scripts/argo_apps.py
+++ gitlab-to-argo/scripts/argo_apps.py
@@ -15,108 +15,108 @@ import gitlab
 import dateutil.parser
 
 g = gitlab.Gitlab(
-        params["gitlab_url"],
-        private_token=params["gitlab_token"]
+    params["gitlab_base_url"],
+    private_token=params["gitlab_private_token"]
 )
 
 
 def archive_project(project):
 
-        print("Archiving project")
+        print("Archiving project...")
 
 
 def apply_project(project):
 
-        print("Apply project")
+        print("Applying project...")
 
 
 def should_process_project(project):
 
-        valid_cloud_providers = [ "ec2", "crc" ]
+    valid_cloud_providers = [ "ec2", "crc" ]
 
-        repository_tree = project.repository_tree(all=True)
-        engagement_file = [f for f in repository_tree if f["path"] == 'engagement.json']
+    repository_tree = project.repository_tree(all=True)
+    engagement_file = [f for f in repository_tree if f["path"] == 'engagement.json']
 
-        if not len(engagement_file) == 1:
+    if not len(engagement_file) == 1:
 
-                # Engagement file doesn't exist
-                print("The Engagement file does not exist!")
-                return False
+        # Engagement file doesn't exist
+        print("The Engagement file does not exist!")
+        return False
 
-        engagement = json.loads(project.files.get('engagement.json', "master").decode())
+    engagement = json.loads(project.files.get('engagement.json', "master").decode())
 
-        print("Checking Engagement: %s - %s"
+    print("Checking Engagement: %s - %s"
         %(engagement.get('customer_name', 'Undefined'),
         engagement.get('project_name', 'Undefined')))
 
-        if not ("launch" in engagement and "launched_date_time" in engagement["launch"]):
+    if not ("launch" in engagement and "launched_date_time" in engagement["launch"]):
 
-                print("Engagement file doesn't have the correct launch data in it")
-                return False
+        print("Engagement file doesn't have the correct launch data in it")
+        return False
 
-        if "archive_date" in engagement:
+    if "archive_date" in engagement:
 
-                try:
+        try:
 
-                        archive_date_ts = dateutil.parser.parse(engagement["archive_date"]).timestamp()
+            archive_date_ts = dateutil.parser.parse(engagement["archive_date"]).timestamp()
 
-                except ValueError:
+        except ValueError:
 
-                        print("Could not parse archive date: %s" %engagement["archive_date"])
-                        return False
+            print("Could not parse archive date: %s" %engagement["archive_date"])
+            return False
 
-                now_ts = datetime.utcnow().timestamp()
+        now_ts = datetime.utcnow().timestamp()
 
-                if now_ts > archive_date_ts:
+        if now_ts > archive_date_ts:
 
-                        print("Engagement has reached archiving date")
-                        #archive_project()
+            print("Engagement has reached archiving date")
+            #archive_project()
 
+    if not "hosting_environments" in engagement:
 
-        if not "hosting_environments" in engagement:
+        print("Hosting environment is not defined in engagement.json")
+        return False
 
-                print("Hosting environment is not defined in engagement.json")
-                return False
+    if len(engagement["hosting_environments"]) == 0:
 
-        if len(engagement["hosting_environments"]) == 0:
+        print("Hosting environment file is empty")
+        return False
 
-                print("Hosting environment file is empty")
-                return False
+    if not "ocp_cloud_provider_name" in engagement["hosting_environments"][0]:
 
-        if not "ocp_cloud_provider_name" in engagement["hosting_environments"][0]:
+        print("No Cloud Provider name was provided")
+        return False
 
-                print("No Cloud Provider name was provided")
-                return False
+    else:
 
-        else:
+        cloud_provider = engagement["hosting_environments"][0]["ocp_cloud_provider_name"]
 
-                cloud_provider = engagement["hosting_environments"][0]["ocp_cloud_provider_name"]
+    if not cloud_provider in valid_cloud_providers:
 
+        print("Engagement is using an unsupported Cloud Provider")
+        return False
 
-        if not cloud_provider in valid_cloud_providers:
-
-                print("Engagement is using an unsupported Cloud Provider")
-                return False
-
-        # :rocket: Ship it!
-        return True
+    # :rocket: Ship it!
+    return True
 
 
 def do_not_process(file):
 
-        print(f"Not processing {file}")
-        os.remove(file)
+    print(f"Not processing {file}")
+    os.remove(file)
 
 
 if __name__ == "__main__":
 
-        files = [f for f in os.listdir(".")]
+    print("Processing files")
 
-        for file in files:
+    files = [f for f in os.listdir(".")]
 
-                project_id = file.partition(".")[0] # The project ID is the filename before ".yml"
-                project = g.projects.get(project_id, lazy=False)
+    for file in files:
 
-        if not should_process_project(project):
+        project_id = file.partition(".")[0] # The project ID is the filename before ".yml"
+        project = g.projects.get(project_id, lazy=False)
 
-                do_not_process(file)
+    if not should_process_project(project):
+
+        do_not_process(file)
